fb1bd8886138a2d3db46c6c05b7c29d2f5a7b15d
morispolanco <morispolanco@gmail.com>
2023-07-27 22:50:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,18 +13,28 @@ def chat_with_gpt3(query, conversation_id):
     }
 
     data = {
-        'model': 'GPT-3.5-Turbo',  # GPT-3.5 model
+        'model': 'GPT-3.5-Turbo',
         'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
                      {'role': 'user', 'content': query, 'user': conversation_id}]
     }
 
-    response = requests.post(url, headers=headers, json=data)
-    return response.json()['choices'][0]['message']['content']
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()  # Verificar si hubo errores en la solicitud
+        response_data = response.json()
+        if 'choices' in response_data and len(response_data['choices']) > 0:
+            return response_data['choices'][0]['message']['content']
+        else:
+            return 'No se obtuvo respuesta del modelo.'
+    except requests.exceptions.RequestException as e:
+        return f'Error en la solicitud: {e}'
+    except KeyError as e:
+        return f'Error al procesar la respuesta: {e}'
 
 def main():
-    st.title('Chat with GPT-3.5')
-    conversation_id = st.text_input('Enter your conversation ID', 'user123')
-    user_query = st.text_input('You:', 'Hello')
+    st.title('Chat con GPT-3.5')
+    conversation_id = st.text_input('Ingrese su ID de conversación', 'user123')
+    user_query = st.text_input('Tú:', 'Hola')
     response = chat_with_gpt3(user_query, conversation_id)
     st.text_area('GPT-3.5:', response)
 
